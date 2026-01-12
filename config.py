91b82187a4205ad24085df0f290d859264aa6783
Akmal-z <akmalzuhairi.az@gmail.com>
2026-01-12 19:06:49 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,15 @@
 # config.py
 
-PERIODS_PER_DAY = 28        # 28 periods per day
-PERIOD_DURATION = 30       # minutes
-SHIFT_LENGTH = 16          # 16 periods = 8 hours
-NUM_DEPARTMENTS = 6
+# Time structure
+PERIODS_PER_DAY = 28          # 28 periods per day
+PERIOD_DURATION = 30          # minutes
+SHIFT_LENGTH = 16             # 16 periods = 8 hours
 
+# Departments
+NUM_DEPARTMENTS = 6
+DEPARTMENTS = [1, 2, 3, 4, 5, 6]
+
+# Days
 DAYS_OF_WEEK = [
     "Monday", "Tuesday", "Wednesday",
     "Thursday", "Friday", "Saturday", "Sunday"
