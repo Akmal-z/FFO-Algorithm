d99ca73b9b92275d94de236390b8bb9680942b9c
Akmal-z <akmalzuhairi.az@gmail.com>
2026-01-12 19:07:22 +0800

    Update fitness.py

--- fitness.py
+++ fitness.py
@@ -1,15 +1,22 @@
 # fitness.py
 
+import numpy as np
 from config import PERIODS_PER_DAY, SHIFT_LENGTH
 
-def evaluate_firefly(firefly, selected_departments):
+def evaluate_firefly(solution, demand):
+    """
+    solution: array of start periods for departments
+    demand: demand value from dataset
+    """
+
     penalty = 0
 
-    for dept in selected_departments:
-        start = firefly[dept - 1]
-
-        # Shift must fit within 28 periods
+    # Constraint: shift must fit in a day
+    for start in solution:
         if start < 0 or start + SHIFT_LENGTH > PERIODS_PER_DAY:
-            penalty += 50
+            penalty += 100
 
-    return -penalty   # Brightness
+    # Cost function (based on dataset demand)
+    cost = demand + penalty
+
+    return cost
