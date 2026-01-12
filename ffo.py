36c5e9f77bab5b777f1f019bc8ae98bc190e71c4
Akmal-z <akmalzuhairi.az@gmail.com>
2026-01-12 19:23:47 +0800

    Update ffo.py

--- ffo.py
+++ ffo.py
@@ -12,7 +12,6 @@ def firefly_optimization(
     alpha,
     beta
 ):
-    # Initialize fireflies
     fireflies = np.random.randint(
         0,
         PERIODS_PER_DAY - SHIFT_LENGTH,
@@ -31,7 +30,6 @@ def firefly_optimization(
                 if brightness[j] < brightness[i]:
                     for dept in selected_departments:
                         idx = dept - 1
-
                         fireflies[i][idx] = (
                             fireflies[i][idx]
                             + beta * (fireflies[j][idx] - fireflies[i][idx])
@@ -51,6 +49,4 @@ def firefly_optimization(
         cost_history.append(brightness.min())
 
     best_index = brightness.argmin()
-    best_solution = fireflies[best_index].astype(int)
-
-    return best_solution, cost_history
+    return fireflies[best_index].astype(int), cost_history
