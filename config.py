e7a42dc77c9f9707f46152d2dfa24ede1b10d311
Akmal-z <akmalzuhairi.az@gmail.com>
2026-01-12 20:59:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,18 +1,13 @@
 # config.py
 
-# Time settings
 PERIODS_PER_DAY = 28
-SHIFT_LENGTH = 14        # SOFT constraint (fixed)
+SHIFT_LENGTH = 14          # soft constraint (fixed)
+MIN_STAFF = 6
 
-# Departments
 NUM_DEPARTMENTS = 6
 DEPARTMENTS = [1, 2, 3, 4, 5, 6]
 
-# Days
 DAYS_OF_WEEK = [
     "Monday", "Tuesday", "Wednesday",
     "Thursday", "Friday", "Saturday", "Sunday"
 ]
-
-# Minimum staff (soft constraint)
-MIN_STAFF = 6
