c23e142bf6d2724291457302a65e31d5c51c4f45
Akmal-z <akmalzuhairi.az@gmail.com>
2026-01-12 18:15:24 +0800

    Add configuration constants for scheduling

---
+++ config.py
@@ -0,0 +1,6 @@
+# config.py
+
+PERIODS_PER_DAY = 28        # Total periods per day
+PERIOD_DURATION = 30       # Minutes per period
+SHIFT_LENGTH = 16          # 16 periods = 8 hours
+NUM_DEPARTMENTS = 6        # Number of departments
